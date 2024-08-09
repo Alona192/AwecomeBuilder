ac324bed586ddc58316a5d794f5ece76f47cd517
Alona <al90nov@gmail.com>
2024-08-09 18:40:08 +0200

    feat: build-item, floor-item done

--- dev/assets/js/modal.js
+++ dev/assets/js/modal.js
@@ -1,90 +1,93 @@
-const modal = document.querySelector('.modal');
+const modal = document.querySelector('.modal-wrapper');
 const triger = document.querySelectorAll('[data-triger-modal]');
-const modalBody = document.querySelector('.modal-body');
 const buildSold = document.querySelectorAll('.sold');
+const modalBody = document.querySelector('.modal-container');
+
 
 const modalArr = [{
-       id: 0,
-       triger: 'sold',
-       title: 'Нажаль всі квартири продані..(',
-       description: 'Вільні квартири знаходяться в будинках,які не продані. Оберіть будь ласка іншу будівлю.',
-       imgUrl: 'assets/img/modal-rejected.png'
-    },
-    {
-        id: 1,
-        triger: 'registrationSuccess',
-        title: 'реєстрація',
-        description: 'Ви успішно зареєструвалися',
-        imgUrl: 'assets/img/modal-accepted.png',
-        markap: `   <form>
-                      <h2 class="modal-title">Реєстрація</h2>
-                      <div class="input-row">
-                         <label for="login">Введіть Ваш логін:</label>
-                         <input type="text" id="login">
-                         <label for="password">Введіть Ваш пароль:</label>
-                         <input type="text" id="password">
-                       </div>
-                       <div class="btn-row">
-                               <button class="btn" data-triger-close="modal">close</button>
-                               <button class="btn">ok</button> 
-                               <button class="btn-close" data-triger-close="modal">x</button>
-                        </div>   
-                  </form>`
-    },
-    {
-        id: 2,
-        triger: 'loginSuccess',
-        title: 'Успішна реєстрація',
-        description: 'Ви успішно зареєструвалися',
-        imgUrl: 'assets/img/modal-accepted.png',
-    },
+    id: 0,
+    triger: 'sold',
+    title: 'Нажаль всі квартири продані..(',
+    description: 'Вільні квартири знаходяться в будинках, які не продані. Оберіть, будь ласка, іншу будівлю, щоб переглянути квартири своєї мрії!)',
+    urlImg: 'assets/img/modal-rejected.png',
+},
+{
+    id: 1,
+    triger: 'registrationSuccess',
+    title: 'Регістрація',
+    description: 'Ви успішно зареєструвалися.',
+    urlImg: 'assets/img/modal-accepted.png',
+    markap: `
+        <form>
+            <div class="input-row">
+                <label for="login">Введіть Ваш логін:</label>
+                <input type="text" id="login">
+                <label for="password">Введіть Ваш пароль:</label>
+                <input type="text" id="password">
+            </div>
+        </form>
+    `,
+},
+{
+    id: 2,
+    triger: 'loginSuccess',
+    title: 'Успішний вхід',
+    description: 'Ви успішно зареєструвалися.',
+    urlImg: 'assets/img/modal-accepted.png',
+},
 ];
-
-const closeModal = () => {
-    modal.classList.remove('show')
-}
-
-modal.addEventListener('click', (event)=> {
-    event.target.classList.remove('show')
-})
 
 const randerModalContent = (modalContent) => {
     if (modalContent.markap) {
-        modalBody.innerHTML = modalContent.markap
+        modalBody.innerHTML = `
+            <h2 class="modal-title">${modalContent.title}</h2>
+            ${modalContent.markap}
+            <div class="button-row">
+                <button class="btn" data-triger-close="modal">Закрити</button>
+                <button class="closed" data-triger-close="modal">X</button>
+            </div>
+        `;
+        
     } else {
-    modalBody.innerHTML = `<h2 class="modal-title">${modalContent.title}</h2>
-                           <p class="modal-description">${modalContent.description}</p>
-                           <img src="${modalContent.imgUrl}" class="modal-img" alt="${modalContent.triger}">
-                           
-                           <div class="btn-row">
-                               <button class="btn" data-triger-close="modal">close</button>
-                               <button class="btn">ok</button> 
-                               <button class="btn-close" data-triger-close="modal">x</button>
-                           </div>`
+        modalBody.innerHTML = `<h2 class="modal-title">${modalContent.title}</h2>
+                            <p class="modal-discription">
+                                ${modalContent.description}
+                            </p>
+                            <div class="modal-img">
+                                <img src="${modalContent.urlImg}" alt="${modalContent.triger}">
+                            </div>
+                            <div class="button-row">
+                                <button class="btn" data-triger-close="modal">Закрити</button>
+                                <button class="closed" data-triger-close="modal">X</button>
+                            </div>`;
     }
-    const trigerClose = document.querySelectorAll('[data-triger-close="modal"]')
-    trigerClose.forEach(btn => {
-        btn.addEventListener('click', closeModal)
-    })
-}
+    
+    
+    const btnClose = document.querySelectorAll('[data-triger-close="modal"]');
+
+    btnClose.forEach(closeItem => {
+        closeItem.addEventListener('click', closeModal);
+    });
+};
 
 const showModal = (modalType) => {
-    const modalData = modalArr.find(item=> item.triger === modalType)
-    console.log(modalData);
-    randerModalContent(modalData)
-    modal.classList.add('show')
-    
-}
+    const modalData = modalArr.find(item => item.triger === modalType);
+
+    randerModalContent(modalData);
+    modal.classList.add('show');
+
+};
+
+const closeModal = () => {
+    modal.classList.remove('show')
+};
 
 const trigerModal = triger.forEach(item => {
-    item.addEventListener('click', () => {
-        const modalType = item.getAttribute('data-triger-modal')
-        showModal(modalType)
+        item.addEventListener('click', () => {
+            const modalType = item.getAttribute('data-triger-modal');
+
+            showModal(modalType);
+        })
     })
-})
-
-
-
-
 
 
